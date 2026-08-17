986943cbf94776eb5519f9a3533ebcb367e5afef
johankadez <dafakurniawan1221@gmail.com>
2026-08-17 23:27:30 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-// Isi dari Firebase Console > Project settings > Your apps > Web app
-export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+const firebaseConfig = {
+  apiKey: "AIzaSyBqtkkXvchgWt8FszgaaH1arIO20YILcOg",
+  authDomain: "insomnia-store-f66eb.firebaseapp.com",
+  projectId: "insomnia-store-f66eb",
+  storageBucket: "insomnia-store-f66eb.firebasestorage.app",
+  messagingSenderId: "774381713580",
+  appId: "1:774381713580:web:96879b4f5d33664c168709",
 };
+export { firebaseConfig };
